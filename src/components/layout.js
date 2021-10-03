301b784b1a4e10d1b8387b27ac2c598060c55ad7
Lidar9k <lidar9k@gmail.com>
2021-10-03 11:01:48 +0900

    mfinish chap4

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,23 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
 
 import * as React from "react"
 import PropTypes from "prop-types"
+import Header from "../components/header"
+import Footer from "../components/footer"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config }　from "@fortawesome/fontawesome-svg-core"
+config.autoAddCss = false
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+const Layout = ({ children }) => (
+  <div>
+    <Header />
+    {children}
+    <Footer />
+  </div>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/404.js
+++ src/pages/404.js
@@ -5,9 +5,7 @@ import Seo from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 style={{ padding: "20vh 0", textAlign: "center" }}>お探しのページが見つかりませんでした</h1>
   </Layout>
 )
 
